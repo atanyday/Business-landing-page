537a7f24e06a2434c946e74925f1189f73ab85cd
Anna Bezrukova <annelia_55@mail.ru>
2017-01-03 21:40:20 +0300

    animation added

--- js/index.js
+++ js/index.js
@@ -13,3 +13,34 @@ $(document).ready(function(){
         }
     });
 });
+
+//animate.css
+$(document).ready(function() {
+    //dog
+    $("#dog").waypoint(function () {
+        $('#dog').toggleClass('animated fadeInRight');
+    }, {offset: '100%'});
+
+    //payment
+    $("#card-left").waypoint(function () {
+        $('#card-left').toggleClass('animated fadeInRight');
+    }, {offset: '100%'});
+    $("#card-right").waypoint(function () {
+        $('#card-right').toggleClass('animated fadeInLeft');
+    }, {offset: '100%'});
+
+    //Iphone
+    $(".iphone-img").waypoint(function () {
+        $('.iphone-img').toggleClass('animated bounceInDown');
+    }, {offset: '100%'});
+    $("#iphone-section-first").waypoint(function () {
+        $('#iphone-section-first').toggleClass('animated fadeInRight');
+    }, {offset: '100%'});
+    $("#iphone-section-second").waypoint(function () {
+        $('#iphone-section-second').toggleClass('animated fadeInRight');
+    }, {offset: '100%'});
+    $("#iphone-section-third").waypoint(function () {
+        $('#iphone-section-third').toggleClass('animated fadeInRight');
+    }, {offset: '100%'});
+});
+
